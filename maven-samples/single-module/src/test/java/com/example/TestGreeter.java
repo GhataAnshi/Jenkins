981a4690a9f1bd421aadd0026400911c3d1c01ae
Ghata Saxena <ghatasaxena27@gmail.com>
2021-09-28 17:25:10 +0530

    Added custom testcase

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Added custom test";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
